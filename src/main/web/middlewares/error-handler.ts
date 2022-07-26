31b0a86dac5a680cc8573fbbbe6e1174c97cb476
Fernando Rodrigues <fernando.silva@compasso.com.br>
2022-07-26 14:05:21 -0300

    rm: either replaced for general error handle

---
+++ src/main/web/middlewares/error-handler.ts
@@ -0,0 +1,14 @@
+import { NextFunction, Request, Response } from "express"
+import { NotFoundError } from "../../shared/errors/not-found-error"
+import { InvalidArgumentError } from "../../shared/errors/invalid-arguments-error"
+
+export default function errorHandler(error: Error, req: Request, res: Response, next: NextFunction) {
+  if(error instanceof InvalidArgumentError) {
+    return res.status(400).json(error.message)
+  } else if(error instanceof NotFoundError) {
+    return res.status(404).json(error.message)
+  } else {
+    return res.status(500).json(error.message)
+  }
+
+}
