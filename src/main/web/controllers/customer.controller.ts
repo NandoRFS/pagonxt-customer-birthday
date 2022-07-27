0bcc33902a293e5f8c9c5a75179a7beaa50b867a
Fernando Rodrigues <fernando.silva@compasso.com.br>
2022-07-27 10:39:35 -0300

    add: customer controller

---
+++ src/main/web/controllers/customer.controller.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express"
+import { container } from "tsyringe"
+import { RegisterCustomerBirthDate } from "../../../main/application/usecases/register-customer-birth-date/register-customer-birth-date"
+import { ListCustomerBirthDays } from "../../../main/application/usecases/list-customer-birthdays.ts/list-customer-birthdays"
+
+export class CustomerController {
+
+  async insert(req: Request, res: Response) {
+    const data = req.body
+
+    const registerCustomerBirthDate = container.resolve(RegisterCustomerBirthDate)
+
+    const response = await registerCustomerBirthDate.execute(data)
+    return res.json(response)
+
+  }
+
+  async find(req: Request, res: Response) {
+    const listCustomerBirthDays = container.resolve(ListCustomerBirthDays)
+    const response = await listCustomerBirthDays.execute()
+    return res.json(response)
+
+  }
+
+}
