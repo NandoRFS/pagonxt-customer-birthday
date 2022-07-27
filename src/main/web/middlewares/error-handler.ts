70da4b3dcb8ba581f9cbdbbd2da541774adf9c7e
Fernando Rodrigues <fernando.silva@compasso.com.br>
2022-07-27 10:09:05 -0300

    up: error handler

--- src/main/web/middlewares/error-handler.ts
+++ src/main/web/middlewares/error-handler.ts
@@ -3,9 +3,9 @@ import { NotFoundError } from "../../shared/errors/not-found-error"
 import { InvalidArgumentError } from "../../shared/errors/invalid-arguments-error"
 
 export default function errorHandler(error: Error, req: Request, res: Response, next: NextFunction) {
-  if(error instanceof InvalidArgumentError) {
+  if (error instanceof InvalidArgumentError) {
     return res.status(400).json(error.message)
-  } else if(error instanceof NotFoundError) {
+  } else if (error instanceof NotFoundError) {
     return res.status(404).json(error.message)
   } else {
     return res.status(500).json(error.message)
