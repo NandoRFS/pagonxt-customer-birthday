b975ff7822c3a7803a520f1ae5c2308e08d57856
Fernando Rodrigues <fernando.silva@compasso.com.br>
2022-07-27 10:10:33 -0300

    up: import database and dotenv config

--- src/main/web/configs/app.ts
+++ src/main/web/configs/app.ts
@@ -1,14 +1,17 @@
+import 'dotenv/config'
+
 import express from 'express'
 import router from '../routes/router'
 import errorHandler from '../middlewares/error-handler'
+import './database'
 
 const app = express()
 
 app.disable('x-powered-by')
 app.disable('etag')
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
-app.use('/pagonxt-custumer-birthday', router)
+app.use(express.urlencoded({ extended: false }))
+app.use('/pagonxt-customer-birthday', router)
 app.use(errorHandler)
 
 export default app
