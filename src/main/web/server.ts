8f12493b247e6d2f80852c1cb488c72a1e241869
Fernando Rodrigues <fernando.silva@compasso.com.br>
2022-07-27 10:11:41 -0300

    up: conditional port statement

--- src/main/web/server.ts
+++ src/main/web/server.ts
@@ -3,7 +3,7 @@ import '../shared/container'
 import 'express-async-errors'
 import app from './configs/app'
 
-const port = process.env.APP_PORT || 3000
+const port = process.env.APP_PORT
 
 export default app.listen(port, () => {
   console.log(`Application loaded on port ${port}`)
