00f6aab23a7296a3b292142235a7daf78b155788
Fernando Rodrigues <fernando.silva@compasso.com.br>
2022-07-28 20:29:04 -0300

    add: customer validator middleware

---
+++ src/main/web/middlewares/customer-validator.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from 'express'
+import Joi from 'joi'
+import { ValidatorError } from '../../../main/shared/errors/validator-error'
+import timezones from 'timezones-list'
+
+export default class CustomerValidator {
+	static async validate(
+		req: Request, res: Response, next: NextFunction,
+	): Promise<Response | void> {
+		const schema = Joi.object({
+			birthDate: Joi.date().less(new Date()),
+			timeZone: Joi.string().required().valid(...timezones.map(tz => tz.tzCode)),
+			customerId: Joi.string().required()
+		})
+
+		const result = schema.validate(req.body, { abortEarly: false })
+		if (result.error) {
+			throw new ValidatorError(result.error.details.map((d) => d.message))
+		}
+		return next()
+	}
+
+}
