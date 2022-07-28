295725baf120ce0145528054ee7344866bd42a3d
Fernando Rodrigues <fernando.silva@compasso.com.br>
2022-07-28 20:35:12 -0300

    add: jest integration and unit configs

--- jest-integration-config.js
+++ jest-integration-config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const config = require('./jest.config')
-config.testMatch = ['**/*.spec.ts']
+config.testMatch = ['**/*.test.ts']
 module.exports = config
