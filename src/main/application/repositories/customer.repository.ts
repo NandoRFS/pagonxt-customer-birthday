fe668cb3e51bebcd3285840ed23644071f7479e7
Fernando Rodrigues <fernando.silva@compasso.com.br>
2022-07-27 22:18:32 -0300

    up: type of find method return

--- src/main/application/repositories/customer.repository.ts
+++ src/main/application/repositories/customer.repository.ts
@@ -2,5 +2,5 @@ import { Customer } from '../../domain/entities/customer/customer'
 
 export interface CustomerRepository {
   insert: (customer: Customer) => Promise<Customer>
-  find: () => Promise<any[]>
+  find: () => Promise<Customer[]>
 }
