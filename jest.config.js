3b8b706fbf3ccf22924f7f92802c60cb1da35b06
Fernando Rodrigues <fernando.silva@compasso.com.br>
2022-07-27 22:22:41 -0300

    add: jest setup and config

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+const path = require('path')
+
+
+module.exports = {
+  rootDir: path.resolve(__dirname, './'),
+  collectCoverageFrom: [
+    '<rootDir>/src/main/**/*.ts',
+    '!<rootDir>/src/main/web/**',
+    '!**/tests/**/*.ts'
+  ],
+  coverageDirectory: process.env.JEST_CLOVER_OUTPUT_DIR,
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  setupFiles: ['dotenv/config']
+}
