babc0ce1e9731c9e6a9704c2316093d0b6898c36
Fernando Rodrigues <fernando.silva@compasso.com.br>
2022-07-28 20:26:23 -0300

    refactor: customer entity class into interface

--- src/main/domain/entities/customer/customer.ts
+++ src/main/domain/entities/customer/customer.ts
@@ -1,30 +1,5 @@
-import { BirthDate } from "./birth-date"
-import { CustomerData } from "./customer-data"
-import { CustomerId } from "./customer-id"
-import { TimeZone } from "./timezone"
-
-export class Customer {
+export interface Customer {
   birthDate: Date
   timeZone: string
   customerId: string
-
-  private constructor(birthDate: Date, timeZone: string, customerId: string) {
-    this.birthDate = birthDate
-    this.timeZone = timeZone
-    this.customerId = customerId
-    Object.freeze(this)
-  }
-
-  static create(customerData: CustomerData): Customer {
-    const createBirthDate: BirthDate = BirthDate.create(customerData.birthDate)
-    const createTimezone: TimeZone = TimeZone.create(customerData.timeZone)
-    const createCustomerId: CustomerId = CustomerId.create(customerData.customerId)
-
-    const birthDate = createBirthDate.value
-    const timeZone = createTimezone.value
-    const customerId = createCustomerId.value
-
-    return new Customer(birthDate, timeZone, customerId)
-  }
-
 }
