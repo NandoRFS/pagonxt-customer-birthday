5fdefa610687e26aae3d075d79a0ef171b73e72e
Fernando Rodrigues <fernando.silva@compasso.com.br>
2022-07-28 20:28:10 -0300

    fix: remove imports

--- src/main/web/server.ts
+++ src/main/web/server.ts
@@ -1,10 +1,8 @@
 import 'reflect-metadata'
-import '../shared/container'
-import 'express-async-errors'
 import app from './configs/app'
 
 const port = process.env.APP_PORT
 
 export default app.listen(port, () => {
-  console.log(`Application loaded on port ${port}`)
+	console.log(`Application loaded on port ${port}`)
 })
