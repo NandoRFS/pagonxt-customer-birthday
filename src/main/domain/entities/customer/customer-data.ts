518adc7a320b49bde317325a65fe33b227270d07
Fernando Rodrigues <fernando.silva@compasso.com.br>
2022-07-28 20:19:53 -0300

    refactor: remove entity validations and add Joi

--- src/main/domain/entities/customer/customer-data.ts
+++
@@ -1,5 +0,0 @@
-export interface CustomerData {
-  birthDate: Date
-  timeZone: string
-  customerId: string
-}
--- src/main/web/routes/customer.routes.ts
+++ src/main/web/routes/customer.routes.ts
@@ -1,11 +1,12 @@
 import express from 'express'
 import { CustomerController } from '../controllers/customer.controller'
+import CustomerValidator from '../middlewares/customer-validator'
 
 const router = express.Router()
 
 const customerController = new CustomerController()
 
-router.post('/birthday', customerController.insert)
+router.post('/birthday', CustomerValidator.validate, customerController.insert)
 router.get('/birthday', customerController.find)
 
 export default router
