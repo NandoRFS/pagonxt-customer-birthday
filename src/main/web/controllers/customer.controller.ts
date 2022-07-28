aa961b1d6cdd643b60f5c2273085d0a76ab5c130
Fernando Rodrigues <fernando.silva@compasso.com.br>
2022-07-28 20:28:35 -0300

    fix: eslint notes

--- src/main/web/controllers/customer.controller.ts
+++ src/main/web/controllers/customer.controller.ts
@@ -1,25 +1,26 @@
-import { Request, Response } from "express"
-import { container } from "tsyringe"
-import { RegisterCustomerBirthDate } from "../../../main/application/usecases/register-customer-birth-date/register-customer-birth-date"
-import { ListCustomerBirthDays } from "../../../main/application/usecases/list-customer-birthdays.ts/list-customer-birthdays"
+import 'reflect-metadata'
+import { Request, Response } from 'express'
+import { container } from 'tsyringe'
+import { RegisterCustomerBirthDate } from '../../../main/application/usecases/register-customer-birth-date/register-customer-birth-date'
+import { ListCustomerBirthDays } from '../../../main/application/usecases/list-customer-birthdays.ts/list-customer-birthdays'
 
 export class CustomerController {
 
-  async insert(req: Request, res: Response) {
-    const data = req.body
+	async insert(req: Request, res: Response) {
+		const data = req.body
 
-    const registerCustomerBirthDate = container.resolve(RegisterCustomerBirthDate)
+		const registerCustomerBirthDate = container.resolve(RegisterCustomerBirthDate)
 
-    const response = await registerCustomerBirthDate.execute(data)
-    return res.json(response)
+		const response = await registerCustomerBirthDate.execute(data)
+		return res.status(201).json(response)
 
-  }
+	}
 
-  async find(req: Request, res: Response) {
-    const listCustomerBirthDays = container.resolve(ListCustomerBirthDays)
-    const response = await listCustomerBirthDays.execute()
-    return res.json(response)
+	async find(req: Request, res: Response) {
+		const listCustomerBirthDays = container.resolve(ListCustomerBirthDays)
+		const response = await listCustomerBirthDays.execute()
+		return res.status(200).json(response)
 
-  }
+	}
 
 }
