ae185d3d0bdc47be3f567819e755a26a1b43bafe
Fernando Rodrigues <fernando.silva@compasso.com.br>
2022-07-29 08:10:55 -0300

    up: add test for put and delete customer birthday

--- src/tests/integration/custumer.routes.test.ts
+++ src/tests/integration/custumer.routes.test.ts
@@ -1,18 +1,21 @@
 import request from 'supertest'
 import app from '../../main/web/configs/app'
-
-import { disconnect, dropCollection } from '../../main/web/configs/database'
+import '../../main/web/configs/dotenv'
+import { disconnect, dropCollection, connect } from '../../main/web/configs/database'
 
 const contextApiPath = '/pagonxt-customer-birthday'
 
 describe('Customer route /customer', () => {
 
+	beforeAll(async () => {
+		console.log('process', process.env.URI)
+		await connect()
+	})
+
 	afterAll((done) => {
 		dropCollection('customers')
 		disconnect(done)
 	})
-
-	// TODO fazer post antes do get e usar como retorno do GET para validar new Date() + 1 day
 
 	test('[GET - /birthday] Should return', async () => {
 		const response = await request(app).get(`${contextApiPath}/customer/birthday`)
@@ -22,16 +25,45 @@ describe('Customer route /customer', () => {
 	test('[POST - /birthday] Should return', async () => {
 		const response = await request(app)
 			.post(`${contextApiPath}/customer/birthday`)
-			.send({ birthDate: '1990-07-29', timeZone: 'Asia/Tokyo', customerId: '12121121211' })
+			.send({ birthDate: '1990-07-29', timeZone: 'Asia/Tokyo', customerId: '123' })
 
 		expect(response.statusCode).toBe(201)
 	})
 
-	test('[POST - /birthday] Should return', async () => {
+	test('[POST - /customer/birthday] Should return', async () => {
 		const response = await request(app)
 			.post(`${contextApiPath}/customer/birthday`)
 			.send({ birthDate: '1990-07-29', timeZone: 'Asia/Tokyo' })
 
 		expect(response.statusCode).toEqual(400)
+	})
+
+	test('[PUT - /customer/birthday] Should return', async () => {
+		const customerId = '123'
+		const body = { birthDate: '1990-07-29', timeZone: 'America/Sao_Paulo' }
+		const response = await request(app)
+			.put(`${contextApiPath}/customer/birthday/${customerId}`)
+			.send(body)
+
+		expect(response.statusCode).toBe(204)
+	})
+
+	test('[PUT - /customer/birthday] Should return', async () => {
+		const customerId = 'invalid123'
+		const body = { birthDate: '1990-07-29', timeZone: 'America/Sao_Paulo' }
+		const response = await request(app)
+			.put(`${contextApiPath}/customer/birthday/${customerId}`)
+			.send(body)
+
+		expect(response.statusCode).toBe(404)
+	})
+
+	test('[DELETE - /customer/birthday] Should return', async () => {
+		const customerId = '123'
+		const response = await request(app)
+			.delete(`${contextApiPath}/customer/birthday/${customerId}`)
+
+		expect(response.statusCode).toBe(200)
+
 	})
 })
