7ce681dbb50aead9936da7bc3d5e10fe0f1f92c6
Fernando Rodrigues <fernando.silva@compasso.com.br>
2022-07-29 08:05:35 -0300

    add: customer update body

--- src/main/web/middlewares/customer-validator.ts
+++ src/main/web/middlewares/customer-validator.ts
@@ -4,7 +4,7 @@ import { ValidatorError } from '../../../main/shared/errors/validator-error'
 import timezones from 'timezones-list'
 
 export default class CustomerValidator {
-	static async validate(
+	static async createValidation(
 		req: Request, res: Response, next: NextFunction,
 	): Promise<Response | void> {
 		const schema = Joi.object({
@@ -12,6 +12,21 @@ export default class CustomerValidator {
 			timeZone: Joi.string().required().valid(...timezones.map(tz => tz.tzCode)),
 			customerId: Joi.string().required()
 		})
+
+		const result = schema.validate(req.body, { abortEarly: false })
+		if (result.error) {
+			throw new ValidatorError(result.error.details.map((d) => d.message))
+		}
+		return next()
+	}
+
+	static async updateValidation(
+		req: Request, res: Response, next: NextFunction,
+	): Promise<Response | void> {
+		const schema = Joi.object({
+			birthDate: Joi.date().less(new Date()).optional(),
+			timeZone: Joi.string().optional().valid(...timezones.map(tz => tz.tzCode)),
+		}).or('birthDate', 'timeZone')
 
 		const result = schema.validate(req.body, { abortEarly: false })
 		if (result.error) {
