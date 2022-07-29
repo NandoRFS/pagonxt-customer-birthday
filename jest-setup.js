f2af4d83565b8decdc0f2d41511b08407a3673ac
Fernando Rodrigues <fernando.silva@compasso.com.br>
2022-07-29 08:11:59 -0300

    add: jest setup

---
+++ jest-setup.js
@@ -0,0 +1,3 @@
+process.env.URI = 'mongodb://localhost/pagonxt-test'
+process.env.PORT = 3000
+process.env.JEST_CLOVER_OUTPUT_DIR = './coverage'
--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,16 @@
 const path = require('path')
 
 module.exports = {
-  rootDir: path.resolve(__dirname, './'),
-  collectCoverageFrom: [
-    '<rootDir>/src/main/**/*.ts',
-    '!<rootDir>/src/main/web/**',
-    '!**/tests/**/*.ts'
-  ],
-  coverageDirectory: process.env.JEST_CLOVER_OUTPUT_DIR,
-  testEnvironment: 'node',
-  transform: {
-    '.+\\.ts$': 'ts-jest'
-  },
-  setupFiles: ['dotenv/config']
+	rootDir: path.resolve(__dirname, './'),
+	collectCoverageFrom: [
+		'<rootDir>/src/main/**/*.ts',
+		'!<rootDir>/src/main/web/**',
+		'!**/tests/**/*.ts'
+	],
+	coverageDirectory: process.env.JEST_CLOVER_OUTPUT_DIR,
+	testEnvironment: 'node',
+	transform: {
+		'.+\\.ts$': 'ts-jest'
+	},
+	setupFiles: ['./jest-setup.js']
 }
