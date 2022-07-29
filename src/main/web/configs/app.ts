b7f199e114a1991cc0e79cae58b4d8e79eaf0d62
Fernando Rodrigues <fernando.silva@compasso.com.br>
2022-07-29 08:01:06 -0300

    refactor: move database from app to server

--- src/main/web/configs/app.ts
+++ src/main/web/configs/app.ts
@@ -1,13 +1,11 @@
 import './dotenv'
 import '../../shared/container'
 import 'express-async-errors'
-import * as database from './database'
 import express from 'express'
 import router from '../routes/router'
 import errorHandler from '../middlewares/error-handler'
 const app = express()
 
-database.connect()
 app.disable('x-powered-by')
 app.disable('etag')
 app.use(express.json())
--- src/main/web/server.ts
+++ src/main/web/server.ts
@@ -1,7 +1,10 @@
 import 'reflect-metadata'
 import app from './configs/app'
+import * as database from './configs/database'
 
-const port = process.env.APP_PORT
+const port = process.env.PORT
+
+database.connect()
 
 export default app.listen(port, () => {
 	console.log(`Application loaded on port ${port}`)
