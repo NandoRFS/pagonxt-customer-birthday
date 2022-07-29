b23ac1e96e282e766e88368ba126d753c71ff2d2
Fernando Rodrigues <fernando.silva@compasso.com.br>
2022-07-29 08:03:17 -0300

    add: update and delete methods for customer

--- src/main/application/repositories/customer.repository.ts
+++ src/main/application/repositories/customer.repository.ts
@@ -1,6 +1,9 @@
+import { CustomerUpdate } from '../../domain/entities/customer/custumer-update'
 import { Customer } from '../../domain/entities/customer/customer'
 
 export interface CustomerRepository {
   insert: (customer: Customer) => Promise<Customer>
   find: () => Promise<Customer[]>
+  findOneAndUpdate: (customerId: string, customerUpdate: CustomerUpdate) => Promise<CustomerUpdate>
+  delete: (customerId: string) => Promise<string>
 }
